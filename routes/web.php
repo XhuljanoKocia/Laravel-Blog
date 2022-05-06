380d896a18f61816b8d572c89b544da25f209d1b
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-05-06 09:43:35 +0200

    Test push

--- routes/web.php
+++ routes/web.php
@@ -13,32 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/about', function () {
-//     return "Hi about page";
-// });
-
-// Route::get('/contact', function () {
-//     return "Hi contact page";
-// });
-
-// Route::get('/post/{id}/{name}', function ($id, $name) {
-//     return "This is post number " . $id . " " . $name;
-// });
-
-// Route::get('/admin/posts/example', array('as' => 'admin.home', function () {
-//     $url = route('admin.home');
-
-//     return "This url is " . $url;
-// }));
-
-// We should use the whole url or controller file
-// Route::get('/post/{id}', '\App\Http\Controllers\PostsController@index');
-
-// With resource we target the CRUD functions
-Route::resource('posts', '\App\Http\Controllers\PostsController');
-
-Route::get('contact', '\App\Http\Controllers\PostsController@contact');
+Route::get('/', function () {
+    return view('welcome');
+});
