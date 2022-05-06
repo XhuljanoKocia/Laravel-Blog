c6dd1299d9f49bcf74d5cb3671947ab75bd9cf39
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-05-06 14:31:22 +0200

    Routing Controllers

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// Creating simple routes like about or contact pages
-Route::get('/about', function () {
-    return "This is the about page.";
-});
+// // Creating simple routes like about or contact pages
+// Route::get('/about', function () {
+//     return "This is the about page.";
+// });
 
-Route::get('/contact', function () {
-    return "This is the contact page.";
-});
+// Route::get('/contact', function () {
+//     return "This is the contact page.";
+// });
 
-// We can pass parameters into the routes like for example a post id
-Route::get('/post/{id}/{name}', function ($id, $name) {
-    return "This is post number " . $id . " " . $name;
-});
+// // We can pass parameters into the routes like for example a post id
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "This is post number " . $id . " " . $name;
+// });
 
-// We can name routes to shorten their name if it is too long or complicated to type and we could save it into a variable
-Route::get('/admin/posts/example', array('as'=>'admin.home', function () {
-    $url = route('admin.home');
-    return "The url is " . $url;
-}));
+// // We can name routes to shorten their name if it is too long or complicated to type and we could save it into a variable
+// Route::get('/admin/posts/example', array('as'=>'admin.home', function () {
+//     $url = route('admin.home');
+//     return "The url is " . $url;
+// }));
+
+Route::get('/post', '\App\Http\Controllers\PostsController@index');
