d68656fa2d7376b7e7221f4c60d988bb199aa8f4
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-05-09 11:27:31 +0200

    Master layour setup

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-</head>
-<body>
-    <h1>This is the contact page</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Contact Page</h1>
+@endsection
+
+@section('footer')
+    <script>alert('Welcome')</script>
+@endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Posts</title>
-</head>
-<body>
-    <h1>This is the post with id {{$id}}</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Post Page</h1>
+@endsection
