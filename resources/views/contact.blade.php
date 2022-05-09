8742942739684db520b82e1db9c7f505c0e0825b
Xhuljano Koçia <53895514+XhuljanoKocia@users.noreply.github.com>
2022-05-09 12:20:33 +0200

    Some more blade features

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
     <h1>Contact Page</h1>
+
+    @if(count($people))
+
+        <ul>
+
+        @foreach($people as $person)
+
+            <li>{{$person}}</li>
+
+        @endforeach
+
+        </ul>
+
+    @endif
+
 @endsection
 
 @section('footer')
-    <script>alert('Welcome')</script>
+
+    <!-- <script>alert('Welcome')</script> -->
+
 @endsection
